e5da40267c884d9eb4fe2515d5e15ffdcafc98ff
havr1985 <gavrtel1985@gmail.com>
2023-08-29 19:14:12 +0300

    one day job

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import { fetchPhoto } from "./js/pixabay-api";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
 const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery')
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
+    gallery.innerHTML = '';
     const search = form.searchQuery.value.trim().toLowerCase();
-    fetchPhoto(search);
+    creatPhotos(search);
 }
+
+async function creatPhotos(q) {
+    try {
+        const photos = await fetchPhoto(q);
+
+        if (!(photos.hits.length)) {
+            Notify.failure('Не коректний запит.Спробуй ще!');
+            return;
+        }
+        console.log(photos.hits)
+        gallery.insertAdjacentHTML('beforeend', markUp(photos.hits)) 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function markUp(photos) {
+    return photos.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`).join(' ');
+}
+
+
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,11 +4,8 @@ const URL = 'https://pixabay.com/api/';
 const KEY = '39126607-3ae5cf154c5ca3fc6757e3d2b';
 
 export async function fetchPhoto(q) {
-  try {
     const response = await axios.get(`${URL}?key=${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`);
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-    }
+    const data = response.data;
+    return data;
+  } 
 
-}
