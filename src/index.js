373ce11a17bc909757a1e73d9f713205653d4932
havr1985 <gavrtel1985@gmail.com>
2023-08-30 18:56:42 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,36 +1,66 @@
 import { fetchPhoto } from "./js/pixabay-api";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const gallerylightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
 const form = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery')
+const gallery = document.querySelector('.gallery');
+const target = document.querySelector('.js-guard');
+let currentPage = 1;
 
 form.addEventListener('submit', onSubmit);
 
+let options = {
+  root: null,
+  rootMargin: "300px",
+  threshold: 1.0,
+};
+
+
+// let observer = new IntersectionObserver(onLoad, options);
 function onSubmit(event) {
     event.preventDefault();
     gallery.innerHTML = '';
     const search = form.searchQuery.value.trim().toLowerCase();
-    creatPhotos(search);
+    loadPhotos(search);
 }
 
-async function creatPhotos(q) {
-    try {
+// function onLoad(entries, observer) {
+//   entries.forEach(async (entry) => {
+//     if (entry.isIntersecting)
+//       currentPage += 1;
+//     loadPhotos(q, currentPage);
+//   }
+//   )}
+
+
+
+
+
+   async function loadPhotos(q, page){   
+      try {
+      
         const photos = await fetchPhoto(q);
 
         if (!(photos.hits.length)) {
             Notify.failure('Sorry, there are no images matching your search query. Please try again.');
             return;
         }
-        console.log(photos.hits)
-        gallery.insertAdjacentHTML('beforeend', markUp(photos.hits)) 
+      console.log(photos.hits)
+      
+      gallery.insertAdjacentHTML('beforeend', markUp(photos.hits))
+      
     } catch (error) {
         console.log(error);
     }
-}
+} 
+   
 
+  
 function markUp(photos) {
-    return photos.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+    return photos.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<a href="${largeImageURL}"><div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
@@ -46,7 +76,7 @@ function markUp(photos) {
       <b>Downloads ${downloads}</b>
     </p>
   </div>
-</div>`).join(' ');
+</div></a>`).join(' ');
 }
 
 
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const URL = 'https://pixabay.com/api/';
 const KEY = '39126607-3ae5cf154c5ca3fc6757e3d2b';
 
-export async function fetchPhoto(q) {
-    const response = await axios.get(`${URL}?key=${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`);
+export async function fetchPhoto(q, page = 1) {
+    const response = await axios.get(`${URL}?key=${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&${page}&per_page=40`);
     const data = response.data;
     return data;
   } 
